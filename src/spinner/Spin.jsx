9644194b02dfc56bdff06389cd7d9b34aec3a948
deba7 <debayudhbasu@gmail.com>
2024-07-01 16:38:50 +0530

    yo

--- src/spinner/Spin.jsx
+++ src/spinner/Spin.jsx
@@ -12,8 +12,6 @@ function Spin() {
     let [color, setColor] = useState("#ffffff");
     return (
         <div className="sweet-loading">
-          <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
-          <input value={color} onChange={(input) => setColor(input.target.value)} placeholder="Color of the loader" />
     
           <ClipLoader
             color={color}
